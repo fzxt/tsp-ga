35b98d0c29c881e434fbe884f14c085ddb097f9b
fzxt <fahadfzxt@gmail.com>
2018-02-14 21:11:31 -0500

    refactor tests for city

--- tests/tsp/City.spec.ts
+++ tests/tsp/City.spec.ts
@@ -1,15 +1,18 @@
 import { expect } from "chai";
 import "mocha";
+
 import City from "../../src/tsp/City";
 
 describe("City", () => {
-  it("getDistance: should compute correct euclidean distance between two cities", () => {
-    const a: City = new City({ x: 100, y: 100 });
-    const b: City = new City({ x: 200, y: 200 });
+  describe("getDistance", () => {
+    it("should compute correct euclidean distance between two cities", () => {
+      const a: City = new City({ x: 100, y: 100 });
+      const b: City = new City({ x: 200, y: 200 });
 
-    const actual = a.getDistance(b).toFixed(3);
-    const expected = 141.421;
+      const actual = a.getDistance(b);
+      const expected = 141.421;
 
-    expect(parseFloat(actual)).to.equal(expected);
+      expect(actual).to.be.closeTo(expected, 0.01);
+    });
   });
 });
